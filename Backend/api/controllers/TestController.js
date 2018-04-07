fdbcd644223019b2d3727a9c3741221eb4b8027b
Zeus Yousif <Zeus_Yousif@Zeus-MacBook-Pro.local>
2018-04-07 01:46:12 -0700

    Fixed ocr

--- Backend/api/controllers/TestController.js
+++ Backend/api/controllers/TestController.js
@@ -1,14 +1,26 @@
+var vision = require('@google-cloud/vision');
+var cloud = require('google-cloud')
+
+const client = new vision.ImageAnnotatorClient();
+
 /**
- * TestController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
+* TODO(developer): Uncomment the following line before running the sample.
+*/
+const fileName = 'https://i.imgur.com/454S8h9.jpg';
 
+// Read a local image as a text document
+
+// https://i.imgur.com/454S8h9.jpg
 module.exports = {
-
 index: function(req, res){
-  res.send('test')
+    client
+     .documentTextDetection(fileName)
+     .then(results => {
+       const fullTextAnnotation = results[0].fullTextAnnotation;
+       console.log(fullTextAnnotation.text);
+     })
+     .catch(err => {
+       console.error('ERROR:', err);
+     });
 }
-
 };
